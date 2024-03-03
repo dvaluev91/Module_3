431cb5bab971bef4db8c62c97580730b018bd886
dvaluev91 <dvaluev91@yandex.ru>
2024-03-03 12:40:57 +0300

    Добавьте файлы проекта.

---
+++ Module_3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Module_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Denis";
+            Console.WriteLine(MyName);
+        }
+    }
+}
