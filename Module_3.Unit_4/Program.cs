21378c926af0ee665bf8d1182ec1412b84780a50
dvaluev91 <dvaluev91@yandex.ru>
2024-04-19 19:43:21 +0300

    Добавьте файлы проекта.

---
+++ Module_3.Unit_4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Module_3.Unit_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do i have a pet?" + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+        }
+    }
+}
